783de51b52af63dd1250d160d29eedca18a7d423
ghifari226 <ghifari226@gmail.com>
2026-08-07 14:01:09 +0700

    Fix superuser card

--- src/pages/admin/RolesPage.jsx
+++ src/pages/admin/RolesPage.jsx
@@ -363,7 +363,9 @@ export default function RolesPage({ session }) {
                     <div className="role-card__header-actions">
                       {isLocked && (
                         <Tooltip title={t('config.roleLockedNotice')}>
-                          <Lock />
+                          <span className="icon-button icon-button--static">
+                            <Lock />
+                          </span>
                         </Tooltip>
                       )}
                       {isDirty && (
